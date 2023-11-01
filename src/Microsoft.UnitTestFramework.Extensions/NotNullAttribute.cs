f9226a70b10376d599b213ed5e447fdf5ca2eae0
Staffan Gustafsson <sgustafsson@ea.com>
2023-11-01 12:59:28 +0100

    Annotating Asserter.IsNotNull with [NotNull] attribute.
    This helps nullability analysis in test projects.

---
+++ src/Microsoft.UnitTestFramework.Extensions/NotNullAttribute.cs
@@ -0,0 +1,13 @@
+﻿#if NETSTANDARD2_1_OR_GREATER
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    using System;
+    /// <summary>
+    /// PlaceHolder for System.Diagnostics.CodeAnalysis.NotNullAttribute for platforms where that attribute is not available.
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Parameter )]
+    internal class NotNullAttribute : Attribute
+    {
+    }
+}
+#endif
